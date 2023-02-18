e26289ebc2d514a01a04680bd0aaf2a0e6f63365
quooctrangg <nguyenquoctrang188@gmail.com>
2023-02-18 16:26:41 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 
-const app = express()
-
+const ApiError = require('./app/api-error')
 const contactRouter = require('./app/routers/contact.router')
+
+const app = express()
 
 app.use(cors())
 app.use(express.json())
@@ -13,6 +14,17 @@ app.use('/api/contacts', contactRouter)
 app.get("/", (req, res) => {
     res.json({
         message: 'Welcome to contact book application.'
+    })
+})
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'))
+})
+
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal server error'
     })
 })
 
